74bf5f5211ab4415ba8d66134eb3821953103716
Brennen Raimer <>
2018-06-23 22:53:27 -0400

    checkpoint 8/23/18

--- PowerSchool_DataViz.py
+++ PowerSchool_DataViz.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtCore, QtGui, QtWidgets, QtWebEngine, QtWebEngineWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets, QtWebEngine, QtWebEngineWidgets, QtPrintSupport
 from bokeh.plotting import figure
 from bokeh.resources import CDN
 from bokeh.embed import file_html
@@ -6,24 +6,72 @@ import pandas
 import os
 import sys
 from pathlib import Path
-from PowerSchool_DataViz_Window import Ui_PowerSchool_DataViz_Window
+from PowerSchool_DataViz_UI import Ui_PowerSchool_DataViz
+import PowerSchool_DataViz_Resources
 
-class PowerSchool_DataViz_Window(QtWidgets.QMainWindow):
-    def __init__(self):
+class AboutWindow(QtWidgets.QMessageBox):
+    def __init__(self,parent=None,description="",license=""):
+        super().__init__(parent)
+        self.setStandardButtons(self.Ok)
+        license_button=QtWidgets.QPushButton(text="License")
+        self.addButton(license_button,self.HelpRole)
+        license_button.pressed.connect(self.__show_license)
+        self.setText(description)
+        self.license=license
+        self.show()
+
+    def __show_license(self):
+        license_window=QtWidgets.QTextEdit(self.license,self)
+        license_window.setReadOnly(True)
+        return license_window
+        
+
+
+
+class PowerSchool_DataViz(QtWidgets.QMainWindow):
+    def __init__(self,parent=None):
+        global app
+        self.app=app
         #initialize the super class
-        super().__init__()
-        Ui_PowerSchool_DataViz_Window().setupUi(self)
+        super().__init__(parent)
+        self.gui=Ui_PowerSchool_DataViz()
+        self.gui.setupUi(self)
+        self.gui.actionNew_Window.triggered.connect(self.__new_window)
+        self.gui.actionQuit.triggered.connect(self.app.closeAllWindows)
+        self.gui.actionOpen.triggered.connect(self.__open_file)
+        self.gui.actionSave_Graph_As.triggered.connect(self.__save_graph)
+        self.gui.actionPrint.triggered.connect(self.__print)
+        self.gui.actionAbout_PowerSchool_DataViz.triggered.connect(self.__about)
+        self.gui.actionAbout_Qt.triggered.connect(self.app.aboutQt)
         self.show()
 
 
+    def __new_window(self):
+        return PowerSchool_DataViz(parent=self)
+
     def __open_file(self):
-        dialog_box=QtWidgets.QFileDialog(self,directory=Path.home(),filter="CSV Files (*.csv)")
         try:
-            self.__input_data=pandas.read_csv(dialog_box.getOpenFileName())
+            self.__input_data=pandas.read_csv(QtWidgets.QFileDialog().getOpenFileName(parent=self,directory=str(Path.home()),filter="CSV Files (*.csv)"))
         except:
             return
-        self.__create_plot()
-        self.__populate_table()
+        #self.__create_plot()
+        #self.__populate_table()
+    
+    def __save_graph(self):
+        filename=QtWidgets.QFileDialog().getSaveFileName(parent=self,directory=str(Path.home()))
+        print(filename)
+
+    def __print(self):
+        QtPrintSupport.QPrintDialog(parent=self).open()
+
+    def __about(self):
+        name=self.app.applicationName()+"\n\n"
+        description="A tool for viewing PowerSchool behavior incident data interactively & graphically\n\nCopyright (c) 2018 Brennen Raimer"
+        file = QtCore.QFile(":/text/LICENSE")
+        if file.open(QtCore.QFile.ReadOnly):
+            license = str(file.readAll(), 'utf-8')
+            file.close()
+        return AboutWindow(parent=self,description=description,license=license)
 
     def __populate_table(self):
         pass
@@ -39,5 +87,5 @@ class PowerSchool_DataViz_Window(QtWidgets.QMainWindow):
 if __name__=='__main__':
     app=QtWidgets.QApplication(sys.argv)
     app.setApplicationName("PowerSchool DataViz")
-    window=PowerSchool_DataViz_Window()
+    window=PowerSchool_DataViz()
     sys.exit(app.exec_())
